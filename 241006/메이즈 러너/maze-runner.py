2f93b06b4981ace794e264c1e50e87b966ec0094
JJuHS <139028940+JJuHS@users.noreply.github.com>
2024-10-06 21:54:40 +0900

    Title: 메이즈 러너, Time: 216ms, Memory: 25MB, Status: Passed - CodeTree

--- 241006/메이즈 러너/maze-runner.py
+++ 241006/메이즈 러너/maze-runner.py
@@ -9,13 +9,14 @@ input = sys.stdin.readline
 # 2개 이상이면, 위에것 먼저, 왼쪽거 먼저
 # 시계방향 90도 회전, 회전한 벽은 내구도 -1
 
+def minus_one(x):
+    return int(x) - 1
+
 direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 n, m, k = map(int, input().split())
 maze = [list(map(int, input().split())) for _ in range(n)]
-survivors = [tuple(map(int, input().split())) for _ in range(m)]
-exit_r, exit_c = map(int, input().split())
-exit_r -= 1
-exit_c -= 1
+survivors = [tuple(map(minus_one, input().split())) for _ in range(m)]
+exit_r, exit_c = map(minus_one, input().split())
 
 res = 0
 
@@ -23,6 +24,7 @@ def move_survivors(dist):
     global survivors
     for idx, p in enumerate(survivors):
         dist_r, dist_c = exit_r - p[0], exit_c - p[1]
+
         if dist_r != 0:
             is_down = 1 if dist_r > 0 else -1
             nr, nc = p[0] + is_down, p[1]
@@ -31,6 +33,7 @@ def move_survivors(dist):
                     survivors[idx] = (nr, nc)
                     dist += 1
                     continue
+
         if dist_c != 0:
             is_right = 1 if dist_c > 0 else -1
             nr, nc = p[0], p[1] + is_right
@@ -67,19 +70,20 @@ def rotate_square(x):
     n_maze = deepcopy(maze)
     n_survivors = []
     n_exit = exit
+    
     for i in range(dist + 1):
         for j in range(dist + 1):
-            if n_maze[start_r + i][start_c + j] > 0:   # 채워
-                n_maze[start_r + i][start_c + j] -= 1   # 채워
-            maze[start_r + i][start_c + j] = n_maze[start_r + dist -j][start_c + i]   # 채워
+            if n_maze[start_r + i][start_c + j] > 0:   
+                n_maze[start_r + i][start_c + j] -= 1 
+            maze[start_r + j][start_c + dist - i] = n_maze[start_r + i][start_c + j]   
 
-            while (start_r + i, start_c + j) in survivors:   # 채워
-                survivors.remove((start_r + i, start_c + j))   # 채워
-                n_survivors.append((start_r + j, start_c + dist - i))   # 채워
+            while (start_r + i, start_c + j) in survivors:   
+                survivors.remove((start_r + i, start_c + j))  
+                n_survivors.append((start_r + j, start_c + dist - i))  
 
-            if (start_r + i, start_c + j) == (exit_r, exit_c):   # 채워
-                n_exit = (start_r + j, start_c + dist - i)   # 채워
-    survivors.extend(n_survivors)
+            if (start_r + i, start_c + j) == (exit_r, exit_c): 
+                n_exit = (start_r + j, start_c + dist - i)  
+    survivors = survivors + n_survivors
     exit_r, exit_c = n_exit
 
 for _ in range(k):
